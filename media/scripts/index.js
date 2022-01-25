1e3cf3cb68366a8be9f0ef5b8d5c2ea9d2b23569
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-01-25 18:33:23 +0530

    Minor Changes in Header

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -10,3 +10,49 @@ lightBtn.addEventListener("click", function() {
 	darkBtn.classList.remove("hide");
 	lightBtn.classList.add("hide");
 });
+
+var homeBtn = document.querySelector(".home");
+var aboutBtn = document.querySelector(".about");
+var skillsBtn = document.querySelector(".skills");
+var qualificationsBtn = document.querySelector(".qualifications");
+var certificatesBtn = document.querySelector(".certificates");
+var projectsBtn = document.querySelector(".projects");
+var contactMeBtn = document.querySelector(".contactme");
+
+var currSelected = homeBtn;
+
+homeBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	homeBtn.classList.add("selected");
+	currSelected = homeBtn;
+});
+
+aboutBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	aboutBtn.classList.add("selected");
+	currSelected = aboutBtn;
+});
+
+skillsBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	skillsBtn.classList.add("selected");
+	currSelected = skillsBtn;
+});
+
+qualificationsBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	qualificationsBtn.classList.add("selected");
+	currSelected = qualificationsBtn;
+});
+
+projectsBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	projectsBtn.classList.add("selected");
+	currSelected = projectsBtn;
+});
+
+contactMeBtn.addEventListener("click", function() {
+	currSelected.classList.remove("selected");
+	contactMeBtn.classList.add("selected");
+	currSelected = contactMeBtn;
+});
