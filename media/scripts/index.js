6240e8a3dc5fe99f60f5f38af79a611409a30507
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-02-05 14:46:55 +0530

    Added changes

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -83,7 +83,7 @@ contactMeBtn.addEventListener("click", function() {
 });
 
 const typed = new Typed('.type', {
-	strings: ['a Web Developer.', 'an Android Developer.', 'a Space Enthusiast.', 'a Speed Cuber.', 'a Gamer.'],
+	strings: ['a Web Developer.', 'an Android Developer.', 'a C++ Programmer.', 'a Space Enthusiast.', 'a Speed Cuber.', 'a Gamer.'],
   	smartBackspace: true,
   	startDelay: 1000,
   	typeSpeed: 50,
