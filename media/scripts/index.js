597a29fefe945f92f7bc1838129c30768013ba0a
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-03-06 17:19:05 +0530

    Added few changes and added learn more option

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -330,6 +330,21 @@ function changeimg() {
   }
 }
 
+const learnMore = document.querySelectorAll(".learn-more");
+const info = document.querySelectorAll(".slide-info p");
+
+for (var i = 0; i < learnMore.length; i++) {
+  learnMore[i].addEventListener("click", function () {
+    if (this.innerHTML == "Show Less") {
+      this.nextElementSibling.style.display = "none";
+      this.innerHTML = "Learn More";
+    } else {
+      this.nextElementSibling.style.display = "block";
+      this.innerHTML = "Show Less";
+    }
+  });
+}
+
 const form = document.getElementById("contact-form");
 
 async function handleSubmit(event) {
