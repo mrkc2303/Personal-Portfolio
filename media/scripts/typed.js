d1afacb35ae3764915daac77525fec8baa7e2721
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-03-15 00:20:23 +0530

    Added Few Changes

--- media/scripts/typed.js
+++ media/scripts/typed.js
@@ -7,6 +7,7 @@
  *   License(s): MIT
  *
  */
+
 (function webpackUniversalModuleDefinition(root, factory) {
   if (typeof exports === "object" && typeof module === "object")
     module.exports = factory();
